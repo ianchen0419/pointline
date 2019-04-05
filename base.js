cfba886a6b8906e3a19112fa91a5fab239c86b01
YianChen <ianchen0419@gmail.com>
2019-04-05 21:39:48 +0800

    top page fix

--- base.js
+++ base.js
@@ -1,3 +1,40 @@
+var headerXhr=new XMLHttpRequest();
+var nowPath=location.pathname.split('/').pop();
+
+headerXhr.open('GET', 'header.html', true);
+headerXhr.send();
+
+headerXhr.onreadystatechange=function(){
+	if(headerXhr.readyState==4 && headerXhr.status==200){
+		header.innerHTML = headerXhr.responseText;
+		menuHighlight();
+	}
+};
+
+
+var footerXhr=new XMLHttpRequest();
+
+footerXhr.open('GET', 'footer.html', true);
+footerXhr.send();
+
+footerXhr.onreadystatechange=function(){
+	if(footerXhr.readyState==4 && footerXhr.status==200){
+		footer.innerHTML = footerXhr.responseText;
+	}
+};
+
+
+function menuHighlight(){
+	var visitedMenu=document.querySelector('nav a[href*="' + nowPath + '"]');
+
+	if(visitedMenu){
+		visitedMenu.closest('li').classList.add('active');
+	}else{
+		document.querySelector('nav ul li').classList.add('active');
+	}
+}
+
+
 var slideIndex=0;
 var dots=document.querySelectorAll('.dots-area i');
 
@@ -41,3 +78,12 @@ function changeIndex(idx){
 	slideIndex=idx;
 	sliderMove();
 }
+
+setInterval(function(){
+	if(slideIndex==2){
+		slideIndex=0;
+	}else{
+		slideIndex++;
+	}
+	sliderMove();
+}, 3000);
